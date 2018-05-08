a57368668b48baa0cae84d36a01c5762ff998d96
awang0523@gmail.com <awang0523@gmail.com>
2018-05-08 14:29:11 -0700

    revert

--- blockchain.py
+++ blockchain.py
@@ -1,19 +1,16 @@
+import datetime
 import hashlib
-import json
-from time import time
+import time
 from urllib.parse import urlparse
-from uuid import uuid4
-
 import requests
 
-class Blockchain:
+class Blockchain(object):
     def __init__(self):
-        self.current_transactions = []
         self.chain = []
+        self.cur_products=[]
         self.nodes = set()
 
-        # Create the genesis block
-        self.new_block(previous_hash='1', proof=100)
+        _init_block = self.new_block(prev_hash = '1', proof = 100)
 
     def register_node(self, address):
         """
@@ -22,43 +19,20 @@ class Blockchain:
         """
 
         parsed_url = urlparse(address)
-        if parsed_url.netloc:
-            self.nodes.add(parsed_url.netloc)
-        elif parsed_url.path:
-            # Accepts an URL without scheme like '192.168.0.5:5000'.
-            self.nodes.add(parsed_url.path)
-        else:
-            raise ValueError('Invalid URL')
+        self.nodes.add(parsed_url.netloc)
+        
+    def new_block(self, proof, prev_hash = None):
+        # Creates a new Block and adds it to the chain
+        _new_block = Block()
+        _new_block._init(len(self.chain) + 1)
+        _new_block.prev_hash = prev_hash or self.chain[-1].cur_hash
+        _new_block.proof = proof
 
+        #
+        #_new_block.product = _new_product
 
-    def valid_chain(self, chain):
-        """
-        Determine if a given blockchain is valid
-        :param chain: A blockchain
-        :return: True if valid, False if not
-        """
-
-        prev_block = chain[0]
-        current_index = 1
-
-        while current_index < len(chain):
-            block = chain[current_index]
-            print(f'{prev_block}')
-            print(f'{block}')
-            print("\n-----------\n")
-            # Check that the hash of the block is correct
-            prev_block_hash = self.hash(prev_block)
-            if block['previous_hash'] != prev_block_hash:
-                return False
-
-            # Check that the Proof of Work is correct
-            if not self.valid_proof(prev_block['proof'], block['proof'], prev_block_hash):
-                return False
-
-            prev_block = block
-            current_index += 1
-
-        return True
+        self.chain.append(_new_block)
+        return _new_block
 
     def resolve_conflicts(self):
         """
@@ -92,81 +66,64 @@ class Blockchain:
             return True
 
         return False
-
-    def new_block(self, proof, previous_hash):
-        """
-        Create a new Block in the Blockchain
-        :param proof: The proof given by the Proof of Work algorithm
-        :param previous_hash: Hash of previous Block
-        :return: New Block
-        """
-
-        block = {
-            'index': len(self.chain) + 1,
-            'timestamp': time(),
-            'transactions': self.current_transactions,
-            'proof': proof,
-            'previous_hash': previous_hash or self.hash(self.chain[-1]),
-        }
-
-        # Reset the current list of transactions
-        self.current_transactions = []
-
-        self.chain.append(block)
-        return block
-
-    def new_transaction(self, sender, recipient, amount):
-        """
-        Creates a new transaction to go into the next mined Block
-        :param sender: Address of the Sender
-        :param recipient: Address of the Recipient
-        :param amount: Amount
-        :return: The index of the Block that will hold this transaction
-        """
-        self.current_transactions.append({
-            'sender': sender,
-            'recipient': recipient,
-            'amount': amount,
-        })
-
-        return self.prev_block['index'] + 1
+    
+    def _new_product(self, name, manufacturer):
+        _new_product = {'name':name, 'manufacturer':manufacturer}
+        self.cur_products.append(_new_product)
+        return self.last_block.index + 1
 
     @property
-    def prev_block(self):
+    def last_block(self):
+        # Returns the last Block in the chain
         return self.chain[-1]
 
-    @staticmethod
-    def hash(block):
+    def valid_chain(self, chain):
         """
-        Creates a SHA-256 hash of a Block
-        :param block: Block
+        Determine if a given blockchain is valid
+        :param chain: A blockchain
+        :return: True if valid, False if not
         """
 
-        # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
-        block_string = json.dumps(block, sort_keys=True).encode()
-        return hashlib.sha256(block_string).hexdigest()
+        prev_block = chain[0]
+        current_index = 1
 
-    def proof_of_work(self, prev_block):
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(f'{prev_block}')
+            print(f'{block}')
+            #print("\n-----------\n")
+            # Check that the hash of the block is correct
+            if block.prev_hash != prev_block.cur_hash:
+                return False
+
+            # Check that the Proof of Work is correct
+            if not self.valid_proof(prev_block.proof, block.proof):
+                return False
+
+            prev_block = block
+            current_index += 1
+
+        return True
+    
+    def proof_of_work(self, last_block):
         """
         Simple Proof of Work Algorithm:
          - Find a number p' such that hash(pp') contains leading 4 zeroes
          - Where p is the previous proof, and p' is the new proof
          
-        :param prev_block: <dict> last Block
+        :param last_block: <dict> last Block
         :return: <int>
         """
 
-        last_proof = prev_block['proof']
-        last_hash = self.hash(prev_block)
+        last_proof = last_block.proof
 
         proof = 0
-        while self.valid_proof(last_proof, proof, last_hash) is False:
+        while self.valid_proof(last_proof, proof) is False:
             proof += 1
 
         return proof
-
     @staticmethod
-    def valid_proof(last_proof, proof, last_hash):
+    def valid_proof(last_proof, proof):
         """
         Validates the Proof
         :param last_proof: <int> Previous Proof
@@ -175,12 +132,10 @@ class Blockchain:
         :return: <bool> True if correct, False if not.
         """
 
-        guess = f'{last_proof}{proof}{last_hash}'.encode()
+        guess = f'{last_proof}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == "0000"
-
-
-    
+        
 class Block:
     def __init__(self):
         self.index = -1
@@ -202,8 +157,4 @@ class Block:
         pass
     
     def _show(self):
-        return 'Block<index: {}, timestamp: {}, prev_hash: {}, hash: {}, product: {}>'.format(
-			self.index, self.timestamp, self.prev_hash, self.cur_hash, self.product)
-    
-
-
+        return 'Block<index: {}, timestamp: {}, prev_hash: {}, hash: {}, product: {}>'.format(self.index, self.timestamp, self.prev_hash, self.cur_hash, self.product)
